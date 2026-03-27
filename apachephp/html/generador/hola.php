6f5fa2bf2d822ac471357331bf952ee21c97f13d
morgonups-dot <morgon.ups@gmail.com>
2026-03-26 22:56:35 -0400

    Delete apachephp directory

--- apachephp/html/generador/hola.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <h1><?php echo "¡Hola Mundo!"; ?></h1>
-  </body>
-</html>
--- apachephp/html/generador/template.php
+++
@@ -1,7 +0,0 @@
-<?php $activo = true; ?>
-
-<?php if ($activo): ?>
-    <p style="color: green;">✔ Usuario activo</p>
-<?php else: ?>
-    <p style="color: red;">✘ Usuario inactivo</p>
-<?php endif; ?>
